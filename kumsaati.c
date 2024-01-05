b9fc89df99ddf3de5779b55cd3d637079d48e331
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-05 18:26:52 +0300

    Update kumsaati.c

--- kumsaati.c
+++ kumsaati.c
@@ -17,3 +17,16 @@ for(i=1;i<=h;i++){
         }
         printf("\n");
 }
+for(i=1;i<=h-1;i++){
+
+        for(j=1;j<=h-i-1;j++){
+        	printf(" ");
+        }
+
+        for(j=1;j<=2*i+1;j++){
+        	printf("*");
+        }
+        printf("\n");
+}
+
+return 0;  }
