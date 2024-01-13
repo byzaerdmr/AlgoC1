0ad97abefea64ded32ff0e09ed0d75c2216176d5
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-13 12:45:03 +0300

    Update arrays2.c

--- arrays2.c
+++ arrays2.c
@@ -1,27 +1,33 @@
-#include<stdio.h>
-#include<stdlib.h>
+
 int main() {
-	int array[100];
-	int i,number;
-	int sum;
-	float average;
-	sum=0;
-	printf("Number of elements: ");
-	scanf("%d",&number);
-	for(i=0;i<number;i++){
-		printf("%d. number: ",i+1);
-		scanf("%d",&array[i]);
-	}
-	for(i=0;i<number;i++){
-		printf("\n %d. number: %d \n",i+1,array[i]);
-	}
-	for(i=0;i<number;i++){
-		sum=sum+array[i];
-		average=sum/number ;
-	}
-	printf("\n Sum of array: %d \n", sum);
-	printf(" Average of array: %f ", average);
-	return 0;
+    
+    int array[100];
+    int i, number;
+    int sum = 0;
+    float average;
+
+    printf("Number of elements: ");
+    scanf("%d", &number);
+
+    for (i = 0; i < number; i++) {
+        printf("%d. number: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    for (i = 0; i < number; i++) {
+        printf("\n %d. number: %d \n", i + 1, array[i]);
+    }
+
+    for (i = 0; i < number; i++) {
+        sum = sum + array[i];
+    }
+
+    average = (float)sum / number; // Ortalamayı döngü sonrasında hesapla
+
+    printf("\n Sum of array: %d \n", sum);
+    printf(" Average of array: %f ", average);
+
+    return 0;
 }
 
 /*
