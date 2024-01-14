dc23892a56401fd886a75a981fb2c09cd3e98c6b
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-14 23:15:57 +0300

    Update factoriel.c

--- factoriel.c
+++ factoriel.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
 	int i,n,factorial=1;
 	printf("please enter a number: ");
@@ -7,8 +7,33 @@ int main(){
 	factorial=i*factorial;
 
 	}
-		printf("factorial of %d: %d", n,factorial);
+		printf("factorial of %d : %d", n,factorial);
 	
 	
 	return 0;
+}*/
+ #include <stdio.h>
+
+// Faktöriyel hesaplayan fonksiyonun prototipi
+unsigned long long int factorial(int number);
+
+int main(void) {
+    // 0'dan 21'e kadar olan sayıların faktöriyellerini hesapla ve ekrana yazdır
+    for (int i = 0; i <= 21; ++i) {
+        printf("%d! = %llu\n", i, factorial(i));
+    }
+    
+    return 0;
 }
+
+// Faktöriyel hesaplayan rekürsif fonksiyonun tanımı
+unsigned long long int factorial(int number) {
+    if (number <= 1) {
+        // Temel durum: 0! ve 1! her zaman 1'dir.
+        return 1;
+    } else {
+        // Recursive adım: number! = number * (number - 1)!
+        return (number * factorial(number - 1));
+    }
+}
+
