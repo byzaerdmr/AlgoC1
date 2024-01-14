c9448496d9f016a47bc0943329a76523dd7e9e92
Beyza <114515168+byzaerdmr@users.noreply.github.com>
2024-01-14 22:10:19 +0300

    Create arrays3%p.c

---
+++ arrays3%p.c
@@ -0,0 +1,10 @@
+// Array name is the same as the address of the array’s first element.
+ #include <stdio.h>
+
+ // function main begins program execution
+ int main(void) {
+ char array[5] = ""; // define an array of size 5
+
+ printf(" array = %p\n&array[0] = %p\n &array = %p\n",array, &array[0], &array); 
+ return 0;
+ } 
